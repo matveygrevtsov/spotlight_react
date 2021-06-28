14090e6deadd38be675c9d4f2f47379c37576448
matveygrevtsov <matveigrevtsov@yandex.ru>
2021-06-28 15:53:13 +0300

    Skins updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,83 @@
-import React, { useEffect, useRef } from 'react';
-import './App.css';
-import image1 from './images/donut.png';
-import image2 from './images/pizza.png';
-import image3 from './images/banana.png';
-import SpotlightAnimator from './Spotlight';
-import {loadImages} from './utils/images';
+import React, { useEffect, useRef } from 'react'
+import './App.css'
+import image1 from './images/circle.svg';
+import image2 from './images/rectangle.svg';
+import image3 from './images/triangle.svg';
+import image4 from './images/triangle2.svg';
+import SpotlightAnimator from './Spotlight'
+import { loadImages } from './utils/images'
 
 interface Props {
-  images?: string[];
+  images?: string[]
 }
 
-const Spotlight: React.FC<Props> = ({images}) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const Spotlight: React.FC<Props> = ({ images }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
     if (canvasRef.current) {
-      const spotlightAnimator = new SpotlightAnimator({canvas: canvasRef.current});
-      loadImages(images).then(imgs => {
+      const spotlightAnimator = new SpotlightAnimator({
+        canvas: canvasRef.current,
+        color: '#111111',
+      })
+      loadImages(images).then((imgs) => {
         spotlightAnimator.addItem({
-          canvas: spotlightAnimator.canvas,
+          image: imgs[0],
+          itemWidth: 9,
+          wrapX0: 18,
+          wrapY0: 4,
+          wrapWidth: 9,
+          wrapHeight: 30,
+        })
+        spotlightAnimator.addItem({
+          image: imgs[1],
+          itemWidth: 20,
+          wrapX0: 27,
+          wrapY0: 7,
+          wrapWidth: 28,
+          wrapHeight: 23,
+        })
+        spotlightAnimator.addItem({
           image: imgs[0],
           itemWidth: 20,
-          wrapX0: 10,
-          wrapY0: 10,
-          wrapWidth: 30,
+          wrapX0: 55,
+          wrapY0: 3,
+          wrapWidth: 28,
           wrapHeight: 30,
-        });
+        })
         spotlightAnimator.addItem({
-          canvas: spotlightAnimator.canvas,
-          image: imgs[1],
-          itemWidth: 35,
-          wrapX0: 45,
-          wrapY0: 5,
+          image: imgs[2],
+          itemWidth: 40,
+          wrapX0: 30,
+          wrapY0: 34,
           wrapWidth: 40,
-          wrapHeight: 60,
-        });
+          wrapHeight: 66,
+        })
         spotlightAnimator.addItem({
-          canvas: spotlightAnimator.canvas,
           image: imgs[0],
           itemWidth: 5,
-          wrapX0: 90,
-          wrapY0: 20,
+          wrapX0: 20,
+          wrapY0: 70,
           wrapWidth: 10,
-          wrapHeight: 30,
-        });
+          wrapHeight: 20,
+        })
         spotlightAnimator.addItem({
-          canvas: spotlightAnimator.canvas,
-          image: imgs[2],
-          itemWidth: 35,
-          wrapX0: 0,
-          wrapY0: 65,
-          wrapWidth: 50,
-          wrapHeight: 35,
-        });
-        spotlightAnimator.start();
-      });
-      return () => spotlightAnimator.destroy();
+          image: imgs[3],
+          itemWidth: 10,
+          wrapX0: 70,
+          wrapY0: 70,
+          wrapWidth: 10,
+          wrapHeight: 20,
+        })
+        spotlightAnimator.start()
+      })
+      return () => spotlightAnimator.destroy()
     }
-  }, [images]);
-  return <canvas ref={canvasRef} />;
-};
-
-function App() {
-  return <Spotlight images={[image1, image2, image3]} />
+  }, [images])
+  return <canvas ref={canvasRef} />
 }
 
-export default App;
+function App() {
+  return <Spotlight images={[image1, image2, image3, image4]} />
+}
+
+export default App
--- src/Spotlight.ts
+++ src/Spotlight.ts
@@ -1,167 +1,265 @@
-const FPS = 60;
-const period_ms = 1 / FPS;
-const spotlightPower = 1;
-const animationSpeed = 500;
-const spotlightRange = 0.2;
-const spotlightRotationSpeed = 2;
+const FPS = 60
+const periodMs = 1 / FPS
+const spotlightPower = 1
+const animationSpeed = 500
+const spotlightRange = 0.2
+const spotlightRotationSpeed = 2
+
+// TODO: Удалить эту функцию и все её вызовы перед продакшеном
+const plotBorder = (
+  canvas: HTMLCanvasElement,
+  color: string,
+  x0: number,
+  y0: number,
+  x1: number,
+  y1: number,
+): void => {
+  const context = canvas.getContext('2d')
+  if (!context) {
+    return
+  }
+  context.beginPath()
+  context.strokeStyle = color
+  context.moveTo(Math.min(x0, x1), Math.min(y0, y1))
+  context.lineTo(Math.max(x0, x1), Math.min(y0, y1))
+  context.lineTo(Math.max(x0, x1), Math.max(y0, y1))
+  context.lineTo(Math.min(x0, x1), Math.max(y0, y1))
+  context.lineTo(Math.min(x0, x1), Math.min(y0, y1))
+  context.stroke()
+}
 
 interface SpotlightItemProps {
-  image: HTMLImageElement;
-  itemWidth: number;
-  wrapX0: number;
-  wrapY0: number;
-  wrapWidth: number;
-  wrapHeight: number;
-  canvas: HTMLCanvasElement;
+  image: HTMLImageElement
+  itemWidth: number
+  wrapX0: number
+  wrapY0: number
+  wrapWidth: number
+  wrapHeight: number
+  canvas: HTMLCanvasElement
+  color: string
+}
+
+interface SpotlightItemConfig {
+  image: HTMLImageElement
+  itemWidth: number
+  wrapX0: number
+  wrapY0: number
+  wrapWidth: number
+  wrapHeight: number
 }
 
 interface SpotlightAnimatorProps {
-  canvas: HTMLCanvasElement;
+  canvas: HTMLCanvasElement
+  color: string
 }
 
 class SpotlightItem {
-  private readonly canvas: HTMLCanvasElement;
-  private readonly image: HTMLImageElement;
-  private readonly itemWidth: number;
-  private readonly wrapX0: number;
-  private readonly wrapY0: number;
-  private readonly wrapWidth: number;
-  private readonly wrapHeight: number;
-  private readonly context: CanvasRenderingContext2D | null;
-  private t: number;
-  private x: number;
-  private y: number;
-  private width_px: number;
-  private height_px: number;
-  private wrapWidth_px: number;
-  private wrapHeight_px: number;
-  private wrapX0_px: number;
-  private wrapY0_px: number;
+  private readonly color: string
+  private readonly canvas: HTMLCanvasElement
+  private readonly image: HTMLImageElement
+  private readonly itemWidth: number
+  private readonly wrapX0: number
+  private readonly wrapY0: number
+  private readonly wrapWidth: number
+  private readonly wrapHeight: number
+  private readonly context: CanvasRenderingContext2D | null
+  private t: number
+  private x: number
+  private y: number
+  private widthPx: number
+  private heightPx: number
+  private wrapWidthPx: number
+  private wrapHeightPx: number
+  private wrapX0Px: number
+  private wrapY0Px: number
 
   constructor(props: SpotlightItemProps) {
-    this.image = props.image;
-    this.itemWidth = props.itemWidth;
-    this.wrapX0 = props.wrapX0;
-    this.wrapY0 = props.wrapY0;
-    this.wrapWidth = props.wrapWidth;
-    this.wrapHeight = props.wrapHeight;
-    this.canvas = props.canvas;
-    this.context = props.canvas.getContext('2d');
-    this.t = 0;
-    this.width_px = 0;
-    this.height_px = 0;
-    this.wrapHeight_px = 0;
-    this.wrapWidth_px = 0;
-    this.wrapX0_px = 0;
-    this.wrapY0_px = 0;
-    this.calculatePx();
-    this.x = this.getX(this.t);
-    this.y = this.getY(this.t);
+    this.color = props.color
+    this.image = props.image
+    this.itemWidth = props.itemWidth
+    this.wrapX0 = props.wrapX0
+    this.wrapY0 = props.wrapY0
+    this.wrapWidth = props.wrapWidth
+    this.wrapHeight = props.wrapHeight
+    this.canvas = props.canvas
+    this.context = props.canvas.getContext('2d')
+    this.t = 0
+    this.widthPx = 0
+    this.heightPx = 0
+    this.wrapHeightPx = 0
+    this.wrapWidthPx = 0
+    this.wrapX0Px = 0
+    this.wrapY0Px = 0
+    this.calculatePx()
+    this.x = this.getX(this.t)
+    this.y = this.getY(this.t)
   }
 
   private calculatePx() {
-    this.wrapX0_px = (this.canvas.width * this.wrapX0) / 100;
-    this.wrapY0_px = (this.canvas.height * this.wrapY0) / 100;
-    this.wrapHeight_px = (this.wrapHeight / 100) * this.canvas.height;
-    this.wrapWidth_px = (this.wrapWidth / 100) * this.canvas.width;
-    this.width_px = (this.itemWidth / 100) * this.canvas.width;
-    this.height_px = (((this.itemWidth / this.image.width) * this.image.height) / 100) * this.canvas.width;
-    if (this.height_px > this.wrapHeight_px) {
-      this.height_px = this.wrapHeight_px;
-      this.width_px = (this.wrapHeight_px * this.image.width) / this.image.height;
+    this.wrapX0Px = (this.canvas.width * this.wrapX0) / 100
+    this.wrapY0Px = (this.canvas.height * this.wrapY0) / 100
+    this.wrapHeightPx = (this.wrapHeight / 100) * this.canvas.height
+    this.wrapWidthPx = (this.wrapWidth / 100) * this.canvas.width
+    this.widthPx = (this.itemWidth / 100) * this.canvas.width
+    this.heightPx =
+      (((this.itemWidth / this.image.width) * this.image.height) / 100) *
+      this.canvas.width
+    if (this.heightPx > this.wrapHeightPx) {
+      this.heightPx = this.wrapHeightPx
+      this.widthPx = (this.wrapHeightPx * this.image.width) / this.image.height
     }
   }
 
   private getX(t: number) {
-    return ((this.wrapWidth_px - this.width_px) / 2) * (Math.cos(t) + 1) + this.wrapX0_px;
+    return (
+      ((this.wrapWidthPx - this.widthPx) / 2) * (Math.cos(t) + 1) +
+      this.wrapX0Px
+    )
   }
 
   private getY(t: number) {
-    return ((this.wrapHeight_px - this.height_px) / 2) * (Math.sin(t) + 1) + this.wrapY0_px;
+    return (
+      ((this.wrapHeightPx - this.heightPx) / 2) * (Math.sin(t) + 1) +
+      this.wrapY0Px
+    )
   }
 
   private incrementTime() {
-    this.t += (animationSpeed * period_ms) / 1000;
+    this.t += (animationSpeed * periodMs) / 1000
   }
 
   private move() {
-    this.incrementTime();
-    this.calculatePx();
-    this.x = this.getX(this.t);
-    this.y = this.getY(this.t);
+    this.incrementTime()
+    this.calculatePx()
+    this.x = this.getX(this.t)
+    this.y = this.getY(this.t)
+  }
+
+  private getGradient(): CanvasGradient | string {
+    if (!this.context) {
+      return this.color
+    }
+    const centerX = this.x + this.widthPx / 2
+    const centerY = this.y + this.heightPx / 2
+    const gradientRadius = Math.min(this.widthPx, this.heightPx) / 2
+    const gradient = this.context.createRadialGradient(
+      centerX +
+        gradientRadius *
+          spotlightRange *
+          Math.cos(this.t * spotlightRotationSpeed),
+      centerY +
+        gradientRadius *
+          spotlightRange *
+          Math.sin(this.t * spotlightRotationSpeed),
+      gradientRadius,
+      centerX +
+        gradientRadius *
+          spotlightRange *
+          Math.cos(this.t * spotlightRotationSpeed),
+      centerY +
+        gradientRadius *
+          spotlightRange *
+          Math.sin(this.t * spotlightRotationSpeed),
+      0,
+    )
+    gradient.addColorStop(0, this.color)
+    gradient.addColorStop(1 - spotlightPower, this.color)
+    gradient.addColorStop(1, 'transparent')
+    return gradient
   }
 
   private renderSpotlight() {
     if (!this.context) {
-      return;
+      return
     }
-    const center_x = this.x + this.width_px / 2;
-    const center_y = this.y + this.height_px / 2;
-    const R = Math.min(this.width_px, this.height_px) / 2;
-    const gradient = this.context.createRadialGradient(
-      center_x + R * spotlightRange * Math.cos(this.t * spotlightRotationSpeed),
-      center_y + R * spotlightRange * Math.sin(this.t * spotlightRotationSpeed),
-      R,
-      center_x + R * spotlightRange * Math.cos(this.t * spotlightRotationSpeed),
-      center_y + R * spotlightRange * Math.sin(this.t * spotlightRotationSpeed),
-      0,
-    );
-    gradient.addColorStop(0, '#111111');
-    gradient.addColorStop(1 - spotlightPower, '#111111');
-    gradient.addColorStop(1, 'transparent');
-    this.context.fillStyle = gradient;
-    this.context.fillRect(this.x - 1, this.y - 1, Math.ceil(this.width_px) + 1, Math.ceil(this.height_px) + 1);
+    this.context.fillStyle = this.getGradient()
+    this.context.fillRect(
+      this.x - 1,
+      this.y - 1,
+      Math.ceil(this.widthPx) + 1,
+      Math.ceil(this.heightPx) + 1,
+    )
   }
 
   public render() {
     if (!this.context) {
-      return;
+      return
     }
-    this.context.save();
-    this.context.drawImage(this.image, this.x, this.y, this.width_px, this.height_px);
-    this.renderSpotlight();
-    this.context.restore();
-    this.move();
+    this.context.save()
+    this.context.drawImage(
+      this.image,
+      this.x,
+      this.y,
+      this.widthPx,
+      this.heightPx,
+    )
+    this.renderSpotlight()
+    this.context.restore()
+    /*
+    plotBorder(this.canvas, 'red', this.x, this.y, this.x + this.widthPx, this.y + this.heightPx);
+    plotBorder(
+      this.canvas,
+      'red',
+      this.wrapX0Px,
+      this.wrapY0Px,
+      this.wrapX0Px + this.wrapWidthPx,
+      this.wrapY0Px + this.wrapHeightPx,
+    );
+    */
+    this.move()
   }
 }
 
 export default class SpotlightAnimator {
-  public readonly canvas: HTMLCanvasElement;
-  private readonly items: SpotlightItem[];
-  private readonly context: CanvasRenderingContext2D | null;
-  private requestAnimation: number;
+  private readonly canvas: HTMLCanvasElement
+  private readonly color: string
+  private readonly items: SpotlightItem[]
+  private readonly context: CanvasRenderingContext2D | null
+  private requestAnimation: number
 
   constructor(props: SpotlightAnimatorProps) {
-    this.canvas = props.canvas;
-    this.items = [];
-    this.context = props.canvas.getContext('2d');
-    this.requestAnimation = 0;
-    this.setCanvasSize();
-    window.addEventListener('resize', () => this.setCanvasSize());
+    this.canvas = props.canvas
+    this.color = props.color
+    this.items = []
+    this.context = props.canvas.getContext('2d')
+    this.requestAnimation = 0
+    this.setCanvasSize()
+    window.addEventListener('resize', () => this.setCanvasSize())
   }
-  public addItem(props: SpotlightItemProps) {
-    this.items.push(new SpotlightItem(props));
+  public addItem(config: SpotlightItemConfig) {
+    this.items.push(
+      new SpotlightItem({
+        canvas: this.canvas,
+        image: config.image,
+        itemWidth: config.itemWidth,
+        wrapX0: config.wrapX0,
+        wrapY0: config.wrapY0,
+        wrapWidth: config.wrapWidth,
+        wrapHeight: config.wrapHeight,
+        color: this.color,
+      }),
+    )
   }
 
-  public start() {
-    this.requestAnimation = requestAnimationFrame(this.render);
+  private setCanvasSize() {
+    this.canvas.width = window.innerWidth
+    this.canvas.height = window.innerHeight
   }
 
   private render = (timestamp: number): void => {
     if (!this.context) {
-      return;
+      return
     }
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.items.forEach((item) => item.render());
-    this.requestAnimation = requestAnimationFrame(this.render);
-  };
-
-  public destroy() {
-    cancelAnimationFrame(this.requestAnimation);
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.items.forEach((item) => item.render())
+    this.requestAnimation = requestAnimationFrame(this.render)
   }
 
-  private setCanvasSize() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+  public start() {
+    this.requestAnimation = requestAnimationFrame(this.render)
+  }
+
+  public destroy() {
+    cancelAnimationFrame(this.requestAnimation)
   }
 }
